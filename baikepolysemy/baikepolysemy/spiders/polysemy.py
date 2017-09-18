7c487cd7052dd17fb00bccc26363461c2e0a0974
Aaron-qf <294836570@qq.com>
2017-09-18 21:11:36 +0800

    Update polysemy.py

--- baikepolysemy/baikepolysemy/spiders/polysemy.py
+++ baikepolysemy/baikepolysemy/spiders/polysemy.py
@@ -3,7 +3,6 @@ from collections import deque
 from urllib.parse import unquote
 
 import scrapy
-import re
 import json
 
 from baikepolysemy.items import BaikepolysemyItem
@@ -26,35 +25,29 @@ class PolysemySpider(scrapy.Spider):
 
     item_queue = deque()
     item_queue.clear()
-    f = open('d:/json/s.json', 'r', encoding='utf-8')
+    f = open('d:/json/s.json', 'r', encoding='utf-8')#读取json格式的文件
     item_seed = f.readlines()
     for item in item_seed:
-        # print(item)
-        # str = re.match(r'[^\t]+\t([^\t]+)\t[^\t]+\t', item)
-        # item_n = str.group(1)
-        # print(item)
         item_j = json.loads(item)
         item_n = item_j['origin']
-        # print(item_n)
         item_queue.append(item_n)  # 所有入口种子存放在队列之中
     print('all seeds have been processed successfully!')
 
     def start_requests(self):
         first_url = 'http://baike.baidu.com/item/' + self.item_queue.popleft() +'?force=1'  # 弹出队列中第一个item
-        yield scrapy.Request(first_url, callback=self.parse, priority=0,
-                             meta={'priority': 0,'splash': {
-                                 'args': {'lua_source': script, 'wait': 0.5}, 'endpoint': 'execute'}})  # 第一层地址
+        yield scrapy.Request(first_url, callback=self.parse, 
+                             meta={'splash': {'args': {'lua_source': script, 'wait': 0.5}, 'endpoint': 'execute'}})  # 第一层地址
 
+        
     def parse(self, response):
-
-
+        
         if(response.css('.lemmaWgt-subLemmaListTitle').extract_first()!=None):#歧义项
-            label = response.css('.lemmaWgt-lemmaTitle-title h1::text').extract_first()
-            items = response.css('div[class=para][label-module=para] a[target=_blank]')
+            label = response.css('.lemmaWgt-lemmaTitle-title h1::text').extract_first()#标签
+            items = response.css('div[class=para][label-module=para] a[target=_blank]')#获得多个歧义项
             for item in items:
                 polysemy_url = 'http://baike.baidu.com'+item.css('a::attr(href)').extract_first()
                 polysemy_title = item.css('a::text').extract_first()
-                yield scrapy.Request(polysemy_url, callback=self.polysemy_parse, priority=0,
+                yield scrapy.Request(polysemy_url, callback=self.polysemy_parse, 
                                      meta={'priority': 0, 'label': label,
                                            'polysemy_url': polysemy_url, 'polysemy_title': polysemy_title,
                                            'splash': {
@@ -80,16 +73,16 @@ class PolysemySpider(scrapy.Spider):
 
             yield item
 
+        #生成下一搜索项的request
         if(len(self.item_queue)== 0):
             pass
         else:
             next_url = 'http://baike.baidu.com/item/' + self.item_queue.popleft() + '?force=1'  # 弹出队列中下一个item
-            yield scrapy.Request(next_url, callback=self.parse, priority=0,
-                             meta={'priority': 0, 'splash': {
-                                 'args': {'lua_source': script, 'wait': 0.5}, 'endpoint': 'execute'}})  # 下一项
+            yield scrapy.Request(next_url, callback=self.parse, 
+                                meta={'splash': {'args': {'lua_source': script, 'wait': 0.5}, 'endpoint': 'execute'}})  # 下一项
 
 
-    def polysemy_parse(self,response):
+    def polysemy_parse(self,response):#处理歧义项页面
         item = BaikepolysemyItem()
         item['polysemy'] = 1
         item['label'] = response.meta.get('label')
